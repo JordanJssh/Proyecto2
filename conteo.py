4c386dffb2f2913cebcf60c4b020785610960d05
USUARIO15-PC\Usuario01 <katty.montoya24@gmail.com>
2016-11-23 13:24:09 -0500

    guadar conteo de datos

--- conteo.py
+++ conteo.py
@@ -25,25 +25,12 @@ def leertxt():
         #print(caracteres)
         #print(len(linea))
     archi.close()
-print(caracteres)
+    archi=open('Conteo.txt','a')
+    archi.write(str(caracteres))
+    
+    print(caracteres)
 
 leertxt()
 
-def crear():
-    archi=open('Conteo.txt','w')
-    archi.close
-def grabar():
-    archi=open('Conteo.txt','a')
-    archi.write(str(caracteres))
-    archi.close()
-
-def leer():
-    archi=open('Conteo.txt','r')
-    linea=archi.readline()
-    while linea!="":
-        print (linea)
-        linea=archi.readline()
-    archi.close()
-    
 
 
