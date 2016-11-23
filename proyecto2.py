6c346705d7f73ae4ba9b9f26d9ea8c45d41250d3
USUARIO15-PC\Usuario01 <katty.montoya24@gmail.com>
2016-11-23 11:38:43 -0500

    Grabartxt

--- proyecto2.py
+++ proyecto2.py
@@ -4,3 +4,13 @@ print("Integrantes: \nJorge Campoverde\nKatty Montoya\nJordan Sánchez")
 def creatxt():
     archi=open('datos.txt','w')
     archi.close()
+
+def grabartxt():
+    archi=open('datos.txt','a')
+    archi.write('Jorge Campoverde\n')
+    archi.write('Katty Montoya\n')
+    archi.write('Jordan Sanchez\n')
+    archi.close()
+
+creatxt()
+grabartxt()
