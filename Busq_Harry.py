285c9f88d67c62683f7c6a75cc62a8a35df9a746
DESKTOP-FF1JBVS\jorda <jordan_jssh_ @hotmail.com>
2016-11-29 21:12:50 -0500

    Agregue comentarios para entender lo que hace cada linea de codigo. Pero aun no encuentro el error.

--- Busq_Harry.py
+++ Busq_Harry.py
@@ -16,16 +16,29 @@ def grabartxt():
 
 def leertxt():
     repetidas=0
-    palabra='Harry'
-    archi=open ('harry.txt','r')
-    linea=archi.readline()
-    for line in lineas:
-        palabras=line.split(' ')
-        for p in palabra:
-            if p ==palabra:
-                repetidas +=1
+    clave='Harry'
+    archi=open ('harry.txt','r') #Abre el archivo harry
+    linea=archi.readline() # lee la linea del archivo
+    palabras=linea.split(' ')# separa la linea leida quitando los espacios
+    cadena=len(palabras) #cadena es igualada al numero de palabras que fueron separadas en el comando anterior
+    for i in range(cadena-1): # i en el rango de la cadena
+        if palabras[i]==clave: # evalua si la posicion de i de palabras es igual a la clave que es Harry
+            repetidas=repetidas+1 #si encuentra la coinsidencia repetidas aumenta
+    while linea != "": #repite todo lo mencionado antes hasta que no haya lineas
+        linea=archi.readline()
+        palabras=linea.split(' ')
+        cadena=len(palabras)
+        for i in range(cadena):
+            if palabras[i]==clave:
+                repetidas=repetidas+1
+    archi.close()
+    #for line in lineas:
+     #   palabras=line.split(' ')
+      #  for p in clave:
+       #     if p ==clave:
+        #        repetidas +=1
 
-    print("Las palabra Harry se repite:",repetidas)
+    print("Las palabra Harry se repite: ",repetidas)
 
 
 leertxt()
